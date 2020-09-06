4435ebb14f32bd584f3ee70c06dcb9251cf8015b
toda-axia <hiroyuki.toda@axia-works.com>
2020-09-07 00:07:27 +0900

    implement repository

--- lib/screens/article/article_screen_model.dart
+++ lib/screens/article/article_screen_model.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:qiita_sample/data/api/qiita_api_client.dart';
 import 'package:qiita_sample/data/entities/qiita_info.dart';
+import 'package:qiita_sample/screens/article/article_repository.dart';
 
 class ArticleScreenModel extends ChangeNotifier {
-  final api = QiitaApiClient.create();
-  List<QiitaInfo> _articles = List();
 
+  final ArticleRepository _repository = ArticleRepository();
+
+  List<QiitaInfo> _articles = List();
   List<QiitaInfo> get articles => _articles;
 
   Future<void> getFlutterArticle() async {
-    _articles = await api.getFlutterArticles();
+    _articles = await _repository.getFlutterArticle();
     notifyListeners();
   }
 }
