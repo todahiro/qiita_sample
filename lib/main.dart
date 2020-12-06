7089ded9c7bd8a5654f1b19c6ab5c2d131c5599b
toda-axia <hiroyuki.toda@axia-works.com>
2020-12-07 00:36:19 +0900

    improve code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_state_notifier/flutter_state_notifier.dart';
-import 'package:qiita_sample/screens/article/article_repository.dart';
 import 'package:qiita_sample/screens/article/article_screen.dart';
-import 'package:qiita_sample/screens/article/article_state.dart';
-import 'package:qiita_sample/screens/article/article_state_notifier.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,14 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: StateNotifierProvider<ArticleStateNotifier, ArticleState>(
-        create: (_) {
-          return ArticleStateNotifier(
-            ArticleRepository(),
-          );
-        },
-        builder: (_, __) => ArticleScreen(),
-      ),
+      home: ArticleScreen(),
     );
   }
 }
--- lib/screens/article/article_screen.dart
+++ lib/screens/article/article_screen.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:provider/provider.dart';
 import 'package:qiita_sample/data/entities/qiita_info.dart';
 import 'package:qiita_sample/screens/article/article_item.dart';
+import 'package:qiita_sample/screens/article/article_repository.dart';
 import 'package:qiita_sample/screens/article/article_state.dart';
+import 'package:qiita_sample/screens/article/article_state_notifier.dart';
 import 'package:qiita_sample/screens/article_detail/article_detail_screen.dart';
 
 class ArticleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Qiita Sample',
+    return StateNotifierProvider<ArticleStateNotifier, ArticleState>(
+      create: (_) {
+        return ArticleStateNotifier(
+          ArticleRepository(),
+        );
+      },
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Qiita Sample',
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
+        body: _List(),
       ),
-      body: _List(),
     );
   }
 }
@@ -23,11 +33,11 @@ class ArticleScreen extends StatelessWidget {
 class _List extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final articles = context.select((ArticleState model) => model.articles);
+    final state = context.watch<ArticleState>();
     return ListView.builder(
-      itemCount: articles.length,
+      itemCount: state.articles.length,
       itemBuilder: (context, int position) => ArticleItem(
-        qiitaInfo: articles[position],
+        qiitaInfo: state.articles[position],
         onArticleClicked: (qiitaInfo) => _openArticleWebPage(
           context,
           qiitaInfo,
@@ -36,7 +46,7 @@ class _List extends StatelessWidget {
     );
   }
 
-  _openArticleWebPage(
+  void _openArticleWebPage(
       BuildContext context,
       QiitaInfo qiitaInfo,
       ) {
