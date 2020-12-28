b1245c334d0bb914dd92f434fc80a3a432ff961e
toda-axia <hiroyuki.toda@axia-works.com>
2020-12-29 02:09:36 +0900

    show indicator in article_detail_screen

--- lib/screens/article_detail/article_detail_screen.dart
+++ lib/screens/article_detail/article_detail_screen.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:qiita_sample/data/entities/qiita_info.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ArticleDetailScreen extends StatelessWidget {
-  ArticleDetailScreen({@required this.qiitaInfo});
+class ArticleDetailScreen extends StatefulWidget {
+  ArticleDetailScreen({
+    @required this.qiitaInfo,
+  });
 
   final QiitaInfo qiitaInfo;
+
+  @override
+  _ArticleDetailScreenState createState() => _ArticleDetailScreenState();
+}
+
+class _ArticleDetailScreenState extends State<ArticleDetailScreen> {
+  num position = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +22,7 @@ class ArticleDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Expanded(
           child: Text(
-            qiitaInfo.title,
+            widget.qiitaInfo.title,
             overflow: TextOverflow.ellipsis,
             style: TextStyle(
               fontSize: 13,
@@ -21,9 +30,30 @@ class ArticleDetailScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: WebView(
-        initialUrl: qiitaInfo.url,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: IndexedStack(
+        index: position,
+        children: [
+          WebView(
+            initialUrl: widget.qiitaInfo.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageStarted: (_) {
+              setState(() {
+                position = 1;
+              });
+            },
+            onPageFinished: (_) {
+              setState(() {
+                position = 0;
+              });
+            },
+          ),
+          Container(
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
     );
   }
